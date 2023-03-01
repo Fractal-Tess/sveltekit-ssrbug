12b95a4bedfde130da905a9f1fc7a58866afec6d
Fractal-Tess <vgfractal@gmail.com>
2023-03-01 16:35:25 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { browser } from '$app/environment';
+  import 'vidstack/define/media-player.js';
+  if (!browser) console.log('SSR');
+  else console.log('CSR');
+</script>
+
+<div>
+  The vidstack import should NOT be able to render on the dev server because it
+  uses the document object
+</div>
